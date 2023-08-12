8c4d61b38f732fa3b47d495160adc12911a668cd
Lucas <lucasgoncalves08@live.com>
2023-08-12 15:45:12 -0300

    finishing main-img section for tablets

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: [
+    './home.component.css',
+    './styles/main-img-section.css'
+  ]
 })
 export class HomeComponent {
 
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,10 +3,7 @@ import { Component, HostListener } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: [
-    './navbar.component.css',
-    './styles/main-img-section.css'
-  ]
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
 
