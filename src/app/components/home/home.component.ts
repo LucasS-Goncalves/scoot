48887d2e67d491c331dfa1d11d81047a38c5f941
Lucas <lucasgoncalves08@live.com>
2023-08-14 09:38:59 -0300

    finishing steps section layout for mobile

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -5,9 +5,27 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: [
     './home.component.css',
-    './styles/main-img-section.css'
+    './styles/main-img-section.css',
+    './styles/steps-section.css'
   ]
 })
 export class HomeComponent {
 
+  steps = [
+    {
+      iconSrc: '../../../assets/icons/locate.svg',
+      title: 'Locate with app',
+      description: 'Use the app to find the nearest scooter to you. We are continuously placing scooters in the areas with most demand, so one should never be too far away.'
+    },
+    {
+      iconSrc: '../../../assets/icons/scooter.svg',
+      title: 'Pick your scooter',
+      description: 'We show the most important info for the scooters closest to you. So you know how much charge they have left and can see roughly how much it will cost.'
+    },
+    {
+      iconSrc: '../../../assets/icons/ride.svg',
+      title: 'Locate with app',
+      description: 'Scan the QR code and the bike will unlock. Retract the cable lock, put on a helmet, and you’re off! Always lock bikes away from walkways and accessibility ramps.'
+    }
+  ];
 }
