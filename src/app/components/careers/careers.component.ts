0e591da6b7de7833ab7330a1d993edfe1588307f
Lucas <lucasgoncalves08@live.com>
2023-08-16 14:05:26 -0300

    finishing reasons section layout for mobiles

--- src/app/components/careers/careers.component.ts
+++ src/app/components/careers/careers.component.ts
@@ -7,8 +7,29 @@ import { Component } from '@angular/core';
     './careers.component.css',
     './styles/main-img-section.css',
     './styles/mission-section.css',
+    './styles/joinUs-section.css'
   ]
 })
 export class CareersComponent {
 
+  reasons = [
+    {
+      img: '../../../assets/images/our-tech.jpg',
+      number: '01',
+      title: 'Our tech',
+      description: 'We’re using cutting edge technology to drive accessible urban transportation forward. Our fully electric scooters are a joy to ride!'
+    },
+    {
+      img: '../../../assets/images/our-integrity.jpg',
+      number: '02',
+      title: 'Our integrity',
+      description: 'We are fully committed to deliver a great yet safe, sustainable micro-mobility experience in every city we serve.'
+    },
+    {
+      img: '../../../assets/images/our-community.jpg',
+      number: '03',
+      title: 'Our community',
+      description: 'We support every community we serve. All workers are paid a living wage based on their location and are Scoot employees.'
+    },
+  ]
 }
