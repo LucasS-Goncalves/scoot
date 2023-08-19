c484d1fd3c6a79e2ac2a9876861e11747bf30879
Lucas <lucasgoncalves08@live.com>
2023-08-18 21:35:11 -0300

    working on characteristics styles for mobiles/tablets

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -5,7 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './about.component.html',
   styleUrls: [
     './about.component.css',
-    './styles/main-img-section.css'
+    './styles/main-img-section.css',
+    './styles/characteristics-section.css'
   ]
 })
 export class AboutComponent {
